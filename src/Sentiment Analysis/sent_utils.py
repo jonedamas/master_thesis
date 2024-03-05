7b0796c1cd664619bd39030a6c6eef0dba3196e0
Jone <jone.h.d@hotmail.no>
2024-03-05 14:20:39 +0100

    update sent utils.py

--- src/Sentiment Analysis/sent_utils.py
+++ src/Sentiment Analysis/sent_utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from typing import List
+from textblob import TextBlob
 
 def aggregate_score(df: pd.DataFrame, colnames: List[str], frequency: str = 'D') -> pd.DataFrame:
     """
@@ -12,7 +13,27 @@ def aggregate_score(df: pd.DataFrame, colnames: List[str], frequency: str = 'D')
     Returns:
         pd.DataFrame
     """
-    
     resampled_df = df[colnames].resample(frequency).mean().dropna()
 
     return  resampled_df
+
+
+def textblob_sentiment_df(df: pd.DataFrame, index_function, frequency: str = 'D') -> pd.DataFrame:
+    """
+    Returns a dataframe with the sentiment scores of the given dataframe
+
+    Args:
+        df: pd.DataFrame
+        index_function: function
+        frequency: str
+
+    Returns:
+        pd.DataFrame
+    """
+    df[['polarity', 'subjectivity']] = df['headline'].apply(lambda x: pd.Series(TextBlob(x).sentiment))
+
+    sent_df = aggregate_score(df, ['polarity', 'subjectivity'], frequency=frequency)
+
+    sent_df['SV'] = index_function(list(sent_df['polarity'].values))
+
+    return sent_df
