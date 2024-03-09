696f585e4e033f77c05b6d18cd6aeed7242f5c4e
Jone <jone.h.d@hotmail.no>
2024-03-09 13:15:16 +0100

    add bert function

--- src/Sentiment Analysis/sent_utils.py
+++ src/Sentiment Analysis/sent_utils.py
@@ -1,8 +1,18 @@
 import pandas as pd
+import numpy as np
+import torch.nn.functional as F
+from tqdm import tqdm
+import torch
 from typing import List
 from textblob import TextBlob
+from transformers import BertTokenizer, BertForSequenceClassification
 
-def aggregate_score(df: pd.DataFrame, colnames: List[str], frequency: str = 'D') -> pd.DataFrame:
+
+def aggregate_score(
+        df: pd.DataFrame,
+        colnames: List[str],
+        frequency: str = 'D'
+    ) -> pd.DataFrame:
     """
     Aggregates the sentiment scores of the given columns in the dataframe
 
@@ -18,7 +28,31 @@ def aggregate_score(df: pd.DataFrame, colnames: List[str], frequency: str = 'D')
     return  resampled_df
 
 
-def textblob_sentiment_df(df: pd.DataFrame, index_function, beta: float, frequency: str = 'D') -> pd.DataFrame:
+def FinBERT_sentiment(
+        headline: pd.Series,
+        model: BertForSequenceClassification,
+        tokenizer: BertTokenizer
+    ) -> pd.Series:
+
+    input = tokenizer(headline, return_tensors="pt")
+
+    with torch.no_grad():
+        prediction = model(**input)
+
+    probabilities = F.softmax(prediction.logits, dim=-1)
+
+    negative, neutral, positive = probabilities[0]
+    score = (positive - negative).item()
+
+    return score
+
+
+def textblob_sentiment_df(
+        df: pd.DataFrame,
+        index_function,
+        beta: float,
+        frequency: str = 'D'
+    ) -> pd.DataFrame:
     """
     Returns a dataframe with the sentiment scores of the given dataframe
 
@@ -31,6 +65,8 @@ def textblob_sentiment_df(df: pd.DataFrame, index_function, beta: float, frequen
         pd.DataFrame
     """
     df[['polarity', 'subjectivity']] = df['headline'].apply(lambda x: pd.Series(TextBlob(x).sentiment))
+
+    df['polarity'] = df['polarity'] ** 2
 
     sent_df = aggregate_score(df, ['polarity', 'subjectivity'], frequency=frequency)
 
