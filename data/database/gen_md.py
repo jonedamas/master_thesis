6757ac59dcd652412250ea429b3e4f83f5887e59
Jone <jone.h.d@hotmail.no>
2024-03-12 09:41:15 +0100

    add BERT tuning

--- data/database/gen_md.py
+++ data/database/gen_md.py
@@ -1,16 +1,30 @@
-import datetime
+from sqlalchemy import create_engine
+from IPython.display import display, Markdown
+import pandas as pd
+from tabulate import tabulate
 
 def update_markdown():
     # Define the Markdown file path
-    file_path = 'db_status.md'
+    file_path = 'data/database/db_status.md'
 
-    # Define the content to add - here, we're just adding a timestamp
-    content = f"Last updated on: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n"
+    engine = create_engine("sqlite:///C:/Users/joneh/master_thesis/data/database/news.db", echo=False)
 
-    # Open the file in append mode ('a') so we add to it without overwriting existing content
-    with open(file_path, 'a') as file:
-        file.write(content + "\n")  # Write the content with a newline for spacing
+    # get number of rows in the news table
+    query = "SELECT COUNT(*) FROM news"
+    num_rows = pd.read_sql(query, engine).iloc[0, 0]
+
+    # get the first 5 rows of the news table
+    query = "SELECT * FROM news LIMIT 5"
+    df = pd.read_sql(query, engine)
+
+    markdown_table = tabulate(df, headers='keys', tablefmt='pipe', showindex=False)
+
+    # Open the file in write mode ('w') to overwrite existing content
+    with open(file_path, 'w') as file:
+        file.write(f'## Number of rows in News df: **{num_rows}**')
+        file.write('\n\n')
+        file.write(markdown_table)
 
 if __name__ == "__main__":
     update_markdown()
-    print("Markdown file updated.")
+    print("Markdown file overwritten.")
